fa4b84f52a76ae0c8b23d6cc4b489b5ebcd47ca8
U.Goryntsev <zmey666@xaker.ru>
2017-04-19 00:17:09 +0300

    Created A, B, C, D activities

--- app/src/main/java/ru/goryntsev/homework01/B_Activity.java
+++ app/src/main/java/ru/goryntsev/homework01/B_Activity.java
@@ -1,22 +1,22 @@
 package ru.goryntsev.homework01;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class A_Activity extends AppCompatActivity {
+public class B_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_a);
-        Button button = (Button) findViewById(R.id.button_to_b);
+        setContentView(R.layout.activity_b);
+        Button button = (Button) findViewById(R.id.button_to_c);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(A_Activity.this, B_Activity.class));
+                startActivity(new Intent(B_Activity.this, C_Activity.class));
             }
         });
     }
