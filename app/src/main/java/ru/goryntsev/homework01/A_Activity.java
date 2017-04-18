151cc198c4f21f45b3477e4794eded42aa8683ac
U.Goryntsev <zmey666@xaker.ru>
2017-04-18 23:53:32 +0300

    initial

---
+++ app/src/main/java/ru/goryntsev/homework01/A_Activity.java
@@ -0,0 +1,13 @@
+package ru.goryntsev.homework01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class A_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a);
+    }
+}
